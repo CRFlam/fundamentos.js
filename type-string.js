2029835c02e4e64280f024c03160bdc4f8319a36
CRFlam <jjsmithcrf@gmail.com>
2023-01-30 21:47:35 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSeguda456!";
+const stringDeNumeros = "34567";
+
+const citacao =  "Meu nome é ";
+const meuNome = "Jeferson";
+
+
+//concatenação (+)
+
+console.log(citacao + meuNome);
+
+//template string OU template literal
